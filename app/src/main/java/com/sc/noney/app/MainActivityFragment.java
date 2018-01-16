8243a59a67f6a35f07d885a2c45be5691688187a
thisjohn <thisjohn19@gmail.com>
2018-01-16 23:31:36 +0800

    Initial project

---
+++ app/src/main/java/com/sc/noney/app/MainActivityFragment.java
@@ -0,0 +1,24 @@
+package com.sc.noney.app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sc.noney.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
